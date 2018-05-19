8b421c791c288a6a576efb1faaafeca5013cbfe1
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2018-05-19 00:06:08 -0400

    Up to "Making a Second Level" Created simple controls/level

---
+++ Project_Boost/Assets/Scripts/Rocket Ship/Rocket.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 200f;
+    [SerializeField] float mainThrust = 200f;
+
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Fuel":
+                //hit fuel
+                break;
+            default: //dead
+
+                break;
+
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float thrust = mainThrust * Time.deltaTime;
+            rigidBody.AddRelativeForce(Vector3.up * thrust);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+
+
+
+}
