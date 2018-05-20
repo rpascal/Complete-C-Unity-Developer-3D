42b67671cba7a73393cf90dfc6dcc934fee09716
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2018-05-19 23:07:32 -0400

    Adding way path

--- Argon_Assault/Assets/Scripts/MusicPlayer.cs
+++ Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour {
 
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Use this for initialization
     void Start() {
         Invoke("LoadFirstScene", 2f);
