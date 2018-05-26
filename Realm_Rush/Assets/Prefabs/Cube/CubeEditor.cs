d69906849b513432c6a0bf32ff33fd3e67baaee3
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2018-05-26 19:34:27 -0400

    Adding custom cube that can only be placed in a predefined interval of spots

---
+++ Realm_Rush/Assets/Prefabs/Cube/CubeEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
+
+
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+    Vector3 snapPos;
+
+    private void Start() {
+        textMesh = GetComponentInChildren<TextMesh>();
+        snapPos = transform.position;
+    }
+
+    void Update() {
+        HandleMovementSnap();
+    }
+
+    private void HandleMovementSnap() {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+        UpdateText();
+    }
+
+    private void UpdateText() {
+        textMesh.text = $"{snapPos.x/gridSize},{snapPos.z / gridSize}";
+    }
+
+}
